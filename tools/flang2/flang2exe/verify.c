1211872a968c374a7ab928fa12442d188d435e49
Gary Klimowicz <gklimowicz@pobox.com>
2017-05-22 18:35:11 -0700

    Add back IL_DASPSP/IL_MVSPSP code that was incorrectly removed

--- tools/flang2/flang2exe/verify.c
+++ tools/flang2/flang2exe/verify.c
@@ -136,6 +136,8 @@ is_known_bug(ILI_OP opc, int j, ILI_OP j_opc)
     return true;
   if (opc == IL_KADD && o == ILIO_KRLNK && r == ILIA_IR)
     return true;
+  if ((opc == IL_DASPSP || opc == IL_MVSPSP) && o == ILIO_DPLNK && r == ILIA_CS)
+    return true;
   if (opc == IL_ALLOC && o == ILIO_IRLNK && r == ILIA_KR)
     return true;
   if (opc == IL_STKR && o == ILIO_KRLNK && (r == ILIA_IR || r == ILIA_AR) &&
