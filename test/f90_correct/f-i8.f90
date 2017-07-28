41af805295505558cb2d37dad29c9d2a9c25a8e2
Tim Keith <tkeith@nvidia.com>
2017-07-28 14:39:27 -0700

    Add tests for default integer and real sizes.

---
+++ test/f90_correct/f-i8.f90
@@ -0,0 +1,25 @@
+!
+! Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+!
+! Licensed under the Apache License, Version 2.0 (the "License");
+! you may not use this file except in compliance with the License.
+! You may obtain a copy of the License at
+!
+!     http://www.apache.org/licenses/LICENSE-2.0
+!
+! Unless required by applicable law or agreed to in writing, software
+! distributed under the License is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the License for the specific language governing permissions and
+! limitations under the License.
+!
+
+! RUN: %flang -i8 -S -emit-llvm %s -o - 2>&1 | FileCheck %s
+! RUN: %flang -fdefault-integer-8 -S -emit-llvm %s -o - 2>&1 | FileCheck %s
+
+program i
+  integer :: x ! CHECK: alloca i64
+  logical :: c ! CHECK: alloca i64
+  x = 5
+  c = .true.
+end program
