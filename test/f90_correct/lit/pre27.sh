e90658eb00e6d5b5a72c6fee9955cf4a18391f6f
Tim Keith <tkeith@nvidia.com>
2017-09-21 11:53:14 -0700

    Change lit scripts to call a common runmake bash script.

--- test/f90_correct/lit/pre27.sh
+++ test/f90_correct/lit/pre27.sh
@@ -14,22 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 # Shared lit script for each tests. Run bash commands that run tests with make.
 
-# RUN: FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %s | tee %t 
-# RUN: cat %t | FileCheck %s
-
-test_name=${TEST_SRC##*/}  # Strip path.
-test_name=${test_name%.*}  # Strip extension.
-
-echo "Test name: $test_name"
-
-MAKE_FILE=$MAKE_FILE_DIR/makefile
-
-export PATH=$PATH:$(pwd)
-
-make -f $MAKE_FILE HOMEQA=$MAKE_FILE_DIR TEST=$test_name OPT="$FLAGS" build
-make -f $MAKE_FILE HOMEQA=$MAKE_FILE_DIR TEST=$test_name OPT="$FLAGS" run 
-make -f $MAKE_FILE HOMEQA=$MAKE_FILE_DIR TEST=$test_name OPT="$FLAGS" verify
-# CHECK: {{[1-9][0-9]* tests PASSED. 0 tests failed}}
+# RUN: FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %S/runmake | tee %t 
+# RUN: cat %t | FileCheck %S/runmake
