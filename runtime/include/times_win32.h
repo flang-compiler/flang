5a77a7d78b25453e1901c9f8fc85e72381b0f8ce
xoviat <xoviat@users.noreply.github.com>
2017-10-29 14:03:44 -0500

    [runtime/include] add filetime header

---
+++ runtime/include/times_win32.h
@@ -0,0 +1,20 @@
+#ifndef _FLANG_TIMES_WIN32
+#define _FLANG_TIMES_WIN32
+  #include <Windows.h>
+
+  typedef __int64 clock_t;
+
+  typedef struct tms {
+    clock_t tms_utime;  /* user time */
+    clock_t tms_stime;  /* system time */
+    clock_t tms_cutime; /* user time of children */
+    clock_t tms_cstime; /* system time of children */
+  } tms;
+
+  clock_t convert_filetime( const FILETIME *ac_FileTime );
+
+  /*
+    Thin emulation of the unix times function
+  */
+  void times(&tms);
+#endif
