934a2b8d7600a94d1a88e36952239f371ea62982
Mark Leair <leairmark@gmail.com>
2018-05-02 17:27:15 -0700

    Add #if guards and doxygen placeholders in cgraph.h.

--- tools/flang2/flang2exe/cgraph.h
+++ tools/flang2/flang2exe/cgraph.h
@@ -15,18 +15,103 @@
  *
  */
 
+#ifndef CGRAPH_H_
+#define CGRAPH_H_
+
 /** \file
  * \brief Callgraph
  */
 
-#ifndef CGRAPH_H
-#define CGRAPH_H
-
 #include "platform.h"
 
-int get_func_record(int);
-void reset_CGR(void);
+/**
+   \brief ...
+ */
+int cgr_barrier(int func);
+
+/**
+   \brief ...
+ */
+int cgr_modifies(int func_sptr, int object_sptr, int flag);
+
+/**
+   \brief ...
+ */
+int get_func_record(int func_sptr);
+
+/**
+   \brief ...
+ */
+void cgr_add_decl(int sptr);
+
+/**
+   \brief ...
+ */
+void cgr_add(int func);
+
+/**
+   \brief ...
+ */
+void cgr_add_mod(int func_sptr, int object);
+
+/**
+   \brief ...
+ */
+void cgr_attribute(char *attr);
+
+/**
+   \brief ...
+ */
+void cgr_call_func(int func, int caller);
+
+/**
+   \brief ...
+ */
 void cgr_call_node(int func_i, int caller_i);
+
+/**
+   \brief ...
+ */
 void cgr_clean(void);
 
-#endif
+/**
+   \brief ...
+ */
+void cgr_dmp_modifies(void);
+
+/**
+   \brief ...
+ */
+void cgr_dump_decls(int flag);
+
+/**
+   \brief ...
+ */
+void cgr_end(void);
+
+/**
+   \brief ...
+ */
+void cgr_enter_func(int func);
+
+/**
+   \brief ...
+ */
+void cgr_keep(int func);
+
+/**
+   \brief ...
+ */
+void cgr_set_flag(int func, int v);
+
+/**
+   \brief ...
+ */
+void cgr_set_ilmpos(int func, int type, long pos, int start);
+
+/**
+   \brief ...
+ */
+void reset_CGR(void);
+
+#endif // CGRAPH_H_
