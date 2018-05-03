bac4258bd61cbf66907710be9ab1443718e5d413
Mark Leair <leairmark@gmail.com>
2018-05-02 17:16:59 -0700

    Add doxygen to asm_anno.h.

--- tools/flang2/flang2exe/asm_anno.h
+++ tools/flang2/flang2exe/asm_anno.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2002-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,24 +15,40 @@
  *
  */
 
-/** \file
- * \brief Interface to annotation module
- *
- * Define interface to Annotation module: which supports assembly file
- * annotation (-Manno switch).
+#ifndef ASM_ANNO_H_
+#define ASM_ANNO_H_
+
+/**
+   \file
+   \brief Interface to annotation module
+
+   Define interface to Annotation module: which supports assembly file
+   annotation (-Manno switch).
+
+   Annotation is supported by the following functions - generally called from
+   the Scheduler/Code Generator module, iff "flg.anno" is true.
  */
 
-/* Annotation is supported by the following 3 functions - generally
-   called from the Scheduler/Code Generator module,
-   iff "flg.anno" is true:  */
+/**
+   \brief ...
+ */
+void anno_blkcnt(int blkno);
 
-extern void annomod_init(void);
-/*	should be called at the beginning of processing for each user
-    function (but not before "merge_blocks()" is called). */
+/**
+   \brief should be called before emitting assembly code for each basic block
+ */
+void annomod_asm(int blkno);
 
-extern void annomod_asm(int blkno);
-/*	should be called before emitting assembly code for each basic block */
+/**
+   \brief should be called after all code is emitted for the current user
+   function, but before assem_end_func() is called.
+ */
+void annomod_end(void);
 
-extern void annomod_end(void);
-/*	should be called after all code is emitted for the current user
-    function, but before "assem_end_func()" is called. */
+/**
+   \brief should be called at the beginning of processing for each user function
+   (but not before merge_blocks() is called).
+ */
+void annomod_init(void);
+
+#endif
