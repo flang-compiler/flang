8d893d3dcfcf43dd70abcb2a7a4c7b91b27d8552
Gary Klimowicz <gklimowicz@pobox.com>
2018-09-21 04:10:42 -0700

    Inline memory operations for performance

--- runtime/include/memops.h
+++ runtime/include/memops.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2017-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,105 @@
  * \brief Various memory operations
  */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(INLINE_MEMOPS)
+#include <sys/types.h>
+
+static inline void
+__attribute__((always_inline))
+__c_mzero1(char *dest, long cnt)
+{
+  (void) __builtin_memset(dest, 0, (size_t) cnt);
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mzero2(short *dest, long cnt)
+{
+  (void) __builtin_memset(dest, 0, (size_t) cnt * sizeof(short));
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mzero4(int *dest, long cnt)
+{
+  (void) __builtin_memset(dest, 0, (size_t) cnt * sizeof(int));
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mzero8(long long *dest, long cnt)
+{
+  (void) __builtin_memset(dest, 0, (size_t) cnt * sizeof(long long));
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mcopy1(char *dest, char *src, long cnt)
+{
+  (void) __builtin_memcpy(dest, src, (size_t) cnt);
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mcopy2(short *dest, short *src, long cnt)
+{
+  (void) __builtin_memcpy(dest, src, (size_t) cnt * sizeof(short));
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mcopy4(int *dest, int *src, long cnt)
+{
+  (void) __builtin_memcpy(dest, src, (size_t) cnt * sizeof(int));
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mcopy8(long long *dest, long long *src, long cnt)
+{
+  (void) __builtin_memcpy(dest, src, (size_t) cnt * sizeof(long long));
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mset1(char *dest, int value, long cnt)
+{
+  ssize_t i;
+  for (i = 0; i < cnt; ++i)
+    dest[i] = (char) value;
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mset2(short *dest, int value, long cnt)
+{
+  ssize_t i;
+  for (i = 0; i < cnt; ++i)
+    dest[i] = (short) value;
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mset4(int *dest, int value, long cnt)
+{
+  ssize_t i;
+  for (i = 0; i < cnt; ++i)
+    dest[i] = value;
+}
+
+static inline void
+__attribute__((always_inline))
+__c_mset8(long long *dest, long long value, long cnt)
+{
+  ssize_t i;
+  for (i = 0; i < cnt; ++i)
+    dest[i] = (long long) value;
+}
+#else
 void __c_mcopy1(char *dest, char *src, long cnt);
 void __c_mcopy2(short *dest, short *src, long cnt);
 void __c_mcopy4(int *dest, int *src, long cnt);
@@ -33,3 +132,9 @@ void __c_mzero1(char *dest, long cnt);
 void __c_mzero2(short *dest, long cnt);
 void __c_mzero4(int *dest, long cnt);
 void __c_mzero8(long long *dest, long cnt);
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
