250f2629196c96f5eb01dc4548dfb7a65b3ac4e4
Gary Klimowicz <gklimowicz@pobox.com>
2018-11-30 09:46:19 -0800

    Migrate additional correctness ests and supporting files
    make "<N> tests tolerated" output also print a flang-friendly "PASS" message

---
+++ test/f90_correct/src/fs26000_d.f90
@@ -0,0 +1,36 @@
+! Copyright (c) 2017-2018, NVIDIA CORPORATION.  All rights reserved.
+!
+! Licensed under the Apache License, Version 2.0 (the "License");
+! you may not use this file except in compliance with the License.
+! You may obtain a copy of the License at
+!
+!     http://www.apache.org/licenses/LICENSE-2.0
+!
+! Unless required by applicable law or agreed to in writing, software
+! distributed under the License is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the License for the specific language governing permissions and
+! limitations under the License.
+
+! Variation from fs26000_a (integer case)
+! This is complex case.
+! related to a test by Neil Carlson from Los Alamos National Laboratory
+! related to Flang github issue #243
+
+type foo
+  class(*), allocatable :: val
+end type
+type(foo) :: x
+x = foo((1,2))
+
+select type (val => x%val)
+type is (complex)
+  if (val /= (1,2)) then
+    print *, "FAIL 1"
+  else
+    print *, "PASS"
+  endif
+class default
+  print *, "FAIL 2"
+end select
+end
---
+++ test/mp_correct/lit/fs26045.sh
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Shared lit script for each tests. Run bash commands that run tests with make.
+
+# RUN: KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %S/runmake | tee %t 
+# RUN: cat %t | FileCheck %S/runmake
