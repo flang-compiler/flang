ca0c228fc61ff13bef30f8a267ca9ea77c8cc2c9
Gary Klimowicz <gklimowicz@pobox.com>
2019-02-21 15:06:11 -0800

    Updated log (natural logarithm) for x86 targets(AVX2, AVX512, KNL)
    These updates replace the existing routines, both scalar and vector
    with much improved performance over existing functions.

---
+++ runtime/libpgmath/lib/common/logf/fs_log_vector.cpp
@@ -0,0 +1,47 @@
+
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+#if !(defined _CPU)
+#error: please define _CPU - specific suffix to a function name
+#endif
+
+#if !(defined _VL)
+#error: please define _VL - Number of elements per vector register
+#endif
+
+
+#include <immintrin.h>
+#define CONFIG 1
+#if ((_VL) == (4))
+#include "helperavx2_128.h"
+#elif ((_VL) == (8))
+#include "helperavx2.h"
+#elif ((_VL) == (16))
+#include "helperavx512f.h"
+#endif
+
+
+#define _JOIN4(a,b,c,d) a##b##c##d
+#define JOIN4(a,b,c,d) _JOIN4(a,b,c,d)
+
+#define log_vec JOIN4(__fs_log_,_VL,_,_CPU)
+
+extern "C" vfloat log_vec(vfloat const);
+
+#include <log_vec.h>
