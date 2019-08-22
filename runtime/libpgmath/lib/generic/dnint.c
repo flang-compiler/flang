ec0374733174a68b472956d22064cdec4601ffcb
Gary Klimowicz <gklimowicz@pobox.com>
2019-08-22 13:27:30 -0700

    Fix incorrect rounding in ANINT()
    This fix applies only to the "common", architecture
    generic, version of anint/dnint.c
    
    When the floating point argument to ANINT() is on the
    range: [ 2^(number of mantissa bits) .. 2^(number
    of mantissa bits +1) ) The adding of +/-0.5 to
    the argument gets normalied in to the round bit
    and depending on the processor's rounding mode can
    incorrectly affect the input argument.
    
    This fixes the ARM flang TA failures with test 'aint'.

--- runtime/libpgmath/lib/generic/dnint.c
+++ runtime/libpgmath/lib/generic/dnint.c
@@ -16,6 +16,11 @@
  */
 #include "mthdecls.h"
 
+/*
+ * libm's round() could also be used if compiled with:
+ *      _ISOC99_SOURCE || _POSIX_C_SOURCE >= 200112L
+ */
+
 #if     defined(TARGET_LINUX_POWER)
 double
 __mth_i_dnint(double f)
@@ -28,16 +33,46 @@ __mth_i_dnint(double f)
      );
   return x;
 }
+
+#elif   defined(__aarch64__)
+double
+__mth_i_dnint(double f)
+{
+  double r;
+  asm("frinta   %d0, %d1"
+    : "=w"(r)
+    : "w"(f)
+    :);
+  return r;
+}
+
 #else   /* #if     defined(TARGET_LINUX_POWER) */
+#include <math.h>
+#include <ieee754.h>
+
 double
 __mth_i_dnint(double f)
 {
   double x = f;     /* Cases where f == 0.0, or f == NaN */
+  union ieee754_double *u = (union ieee754_double *)&x;
+
+  /*
+   * Depending on the default rounding mode of the processor, the logic
+   * below with modf(f + 0.5) can result in a bogus rounding when 0.5
+   * is normalized such that it falls within the guard or round bits.
+   *
+   * Fast return if the exponent guarantees that the floating point number
+   * is a whole integer.
+   *
+   * This quick exit also catches infinities and NaNs.
+   */
+
+  if (u->ieee.exponent >= IEEE754_DOUBLE_BIAS+52) return x;
 
   if (f > 0.0)
-    (void)modf(f + 0.5f, &x);
+    (void)modf(f + 0.5, &x);
   else if (f < 0.0)
-    (void)modf(f - 0.5f, &x);
+    (void)modf(f - 0.5, &x);
   return x;
 }
 #endif  /* #if     defined(TARGET_LINUX_POWER) */
