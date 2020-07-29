c37f2993f1cc85b8c4077355fc7635af1bf4bfe9
Alok Kumar Sharma <AlokKumar.Sharma@amd.com>
2020-07-29 15:03:46 +0530

    flang gen-exec does not generate DW_AT_associated or DW_AT_allocated
        This patch makes use of commited llvm patch
          [DebugInfo] Support for DW_AT_associated and DW_AT_allocated.
          https://reviews.llvm.org/rG2d10258a31a6d05368dfd4442c4aaa7b54614f1e
    
        This fix is needed for allocatable/pointer arrays.
    
        for pointer array (before allocation/association)
        Before fix
        ------
        (gdb) pt ptr
        type = integer (140737345375288:140737354129776)
        (gdb) p ptr
        value requires 35017956 bytes, which is more than max-value-size
        ------
        After fix
        ------
        (gdb) pt ptr
        type = integer (:)
        (gdb) p ptr
        $1 = <not associated>
        ------
        for allocatable array (before allocation)
        Before fix
        ------
        (gdb) pt arr
        type = integer (140737345375288:140737354129776)
        (gdb) p arr
        value requires 35017956 bytes, which is more than max-value-size
        ------
        After fix
        ------
        (gdb) pt arr
        type = integer, allocatable (:)
        (gdb) p arr
        $1 = <not allocated>
        ------
    
    Testing:
        - check-flang

--- test/debug_info/dertyp_striding.f90
+++ test/debug_info/dertyp_striding.f90
@@ -1,7 +1,7 @@
 !RUN: %flang -g -S -emit-llvm %s -o - | FileCheck %s
 
 !CHECK !DILocalVariable(name: "pvar", scope: {{![0-9]+}}, file: {{![0-9]+}}, type: {{![0-9]+}})
-!CHECK: !DICompositeType(tag: DW_TAG_array_type, baseType: {{![0-9]+}}, size: 64, align: 64, elements: [[ELEM:![0-9]+]], dataLocation: {{![0-9]+}})
+!CHECK: !DICompositeType(tag: DW_TAG_array_type, baseType: {{![0-9]+}}, size: 64, align: 64, elements: [[ELEM:![0-9]+]], dataLocation: {{![0-9]+}}, associated: {{![0-9]+}})
 !CHECK: [[ELEM]] = !{[[ELEM1:![0-9]+]]}
 !CHECK: [[ELEM1]] = !DISubrange(lowerBound: !DIExpression(DW_OP_push_object_address, DW_OP_plus_uconst, 80, DW_OP_deref), upperBound: !DIExpression(DW_OP_push_object_address, DW_OP_plus_uconst, 120, DW_OP_deref), stride: !DIExpression(DW_OP_push_object_address, DW_OP_plus_uconst, 112, DW_OP_deref, DW_OP_push_object_address, DW_OP_plus_uconst, 24, DW_OP_deref, DW_OP_mul))
 program main
