39373c47bb92db96d644db593196aa45ba88fd96
Bryan Chan <bryan.chan@huawei.com>
2020-09-24 08:12:02 -0400

    f90_correct: exclude tests failing with LLVM 10 on OpenPower

--- test/f90_correct/lit/ls07.sh
+++ test/f90_correct/lit/ls07.sh
@@ -7,3 +7,5 @@
 
 # RUN: KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %S/runmake | tee %t 
 # RUN: cat %t | FileCheck %S/runmake
+
+# UNSUPPORTED: powerpc64le-host, ppc64le-host
