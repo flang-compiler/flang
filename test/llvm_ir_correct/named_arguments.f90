flang-compiler/flang

commit aec36b8fec757d9c9328df05da9daa7ab03c8347

Fix private overwrite flag
find_def_in_most_recent_scope() set the private bit to 0 when it found an alias. This was most likely unintended and could cause issues when resolving multiple definitions of the same symbol at a later stage (if a private symbol appeared to be publicly available).

Tests for private flag overwrite added, based on flang github issue #890.

tools/flang1/flang1exe/module.c
@@ -503,7 +503,6 @@ find_def_in_most_recent_scope(int sptr, int save_sem_scope_level)
     if (NMPTRG(sptr1) != NMPTRG(sptr))
       continue;
     if (STYPEG(sptr1) == ST_ALIAS && aliased_sym_visible(sptr1)) {
-      PRIVATEP(sptr1, 0);
       HIDDENP(SYMLKG(sptr1), 0);
     }
     if (STYPEG(sptr1) == ST_ALIAS) {

test/f90_correct/inc/use00.mk
@@ -1,0 +1,20 @@
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+
+########## Make rule for test use00  ########
+
+build:
+	@echo ------------------------------------ building test $(TEST)
+	-$(CC) -c $(CFLAGS) $(SRC)/check.c -o check.$(OBJX)
+	-$(FC) -c $(FFLAGS) $(LDFLAGS) $(SRC)/$(TEST).f90 -o $(TEST).$(OBJX)
+	-$(FC) $(FFLAGS) $(LDFLAGS) $(TEST).$(OBJX) check.$(OBJX) $(LIBS) -o $(TEST).$(EXESUFFIX)
+
+run:
+	@echo ------------------------------------ executing test $(TEST)
+	./$(TEST).$(EXESUFFIX)
+
+verify: ;
+

test/f90_correct/lit/use00.sh
@@ -1,0 +1,9 @@
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Shared lit script for each tests. Run bash commands that run tests with make.
+
+# RUN: KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %S/runmake | tee %t 
+# RUN: cat %t | FileCheck %S/runmake

test/f90_correct/src/use00.f90
@@ -1,0 +1,39 @@
+module f_precisions
+  integer, parameter, public :: f_double = selected_real_kind(15, 307)
+end module f_precisions
+
+module box
+  use f_precisions, gp=>f_double
+  private
+  public :: get_gp
+contains
+  subroutine get_gp(out_val)
+    integer, intent(out) :: out_val
+    out_val = gp
+  end subroutine get_gp
+end module box
+
+module Poisson_Solver
+  use f_precisions
+  integer, parameter, public :: gp=f_double
+end module Poisson_Solver
+
+module module_defs
+  use f_precisions
+  integer, parameter, public :: gp=f_double
+end module module_defs
+
+subroutine PSolver(eh)
+  use module_defs ! This has a public gp
+  use box ! This has a private gp
+  use Poisson_Solver, except_gp => gp ! This has a public gp
+  real(gp), intent(out) :: eh ! Should be able to use gp from module_base
+end subroutine PSolver
+
+program main
+  use box
+  use Poisson_Solver
+  integer :: box_gp
+  call get_gp(box_gp)
+  call check(box_gp, gp, 1) ! box's gp and Solver's gp alias the same variable
+end program

commit 93c7ac429a07891c0c22fac61dfd7bfe90c6df4d

Fix for regression introduced by PR #922 (issue #995).
This commit contains a test case supporting the need for this change.

The fix itself was worked out by Michal Paszta.

Signed-off-by: Paul Osmialowski <pawel.osmialowski@arm.com>

tools/flang1/flang1exe/module.c
@@ -501,9 +501,11 @@ find_def_in_most_recent_scope(int sptr, int save_sem_scope_level)
   SCOPESTACK *scope;
 
   for (sptr1 = first_hash(sptr); sptr1; sptr1 = HASHLKG(sptr1)) {
+    int sptr1_prv_recov = PRIVATEG(sptr1); /* Store the value for recovery */
     if (NMPTRG(sptr1) != NMPTRG(sptr))
       continue;
     if (STYPEG(sptr1) == ST_ALIAS && aliased_sym_visible(sptr1)) {
+      PRIVATEP(sptr1, 0);
       HIDDENP(SYMLKG(sptr1), 0);
     }
     if (STYPEG(sptr1) == ST_ALIAS) {
@@ -550,6 +552,7 @@ find_def_in_most_recent_scope(int sptr, int save_sem_scope_level)
         return sptr1;
       }
     }
+    PRIVATEP(sptr1, sptr1_prv_recov); /* Put back the original value */
   }
   return NOSYM;
 }

test/llvm_ir_correct/named_arguments.f90
@@ -1,0 +1,39 @@
+! Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+! See https://llvm.org/LICENSE.txt for license information.
+! SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+! RUN: %flang -S -emit-llvm %s
+
+MODULE operations_module
+   IMPLICIT NONE
+   PUBLIC :: operation
+   PRIVATE
+   INTERFACE operation
+      MODULE PROCEDURE operation_anytype
+   END INTERFACE
+CONTAINS
+   SUBROUTINE operation_anytype(key)
+      REAL(8), INTENT(IN)                :: key
+   END SUBROUTINE operation_anytype
+END MODULE operations_module
+
+MODULE api_module
+   USE operations_module, ONLY: operation_prv => operation
+   IMPLICIT NONE
+   PUBLIC :: operation
+   PRIVATE
+CONTAINS
+   SUBROUTINE operation(key)
+      REAL(8), INTENT(IN)                               :: key
+   END SUBROUTINE operation
+END MODULE api_module
+
+MODULE methods_module
+   USE api_module, ONLY: operation
+   IMPLICIT NONE
+CONTAINS
+   SUBROUTINE method(key_value)
+      REAL(KIND = 8), INTENT(IN)                          :: key_value
+      CALL operation(key = key_value)
+   END SUBROUTINE method
+END MODULE methods_module