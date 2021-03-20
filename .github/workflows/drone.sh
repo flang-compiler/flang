fc98f0e4acc19df6d97cfadac3d546f3799deb85
xoviat <xoviat@users.noreply.github.com>
2021-03-20 13:16:17 -0500

    fixup

--- .github/workflows/drone.sh
+++ .github/workflows/drone.sh
@@ -1,55 +1,54 @@
-apt update
-apt install -y python3-sphinx cmake curl jq wget
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
+sudo apt install gcc-10 g++-10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 20
+
+sudo apt update
+sudo apt install python3-sphinx
+git --version
 cmake --version
 make --version
 gcc --version
+
 cd ../..
 curl -sL https://api.github.com/repos/flang-compiler/llvm/actions/workflows/build_llvm.yml/runs --output runs_llvm.json
 curl -sL https://api.github.com/repos/flang-compiler/flang-driver/actions/workflows/build_flang-driver.yml/runs --output runs_flang-driver.json
+
 wget --output-document artifacts_llvm `jq -r '.workflow_runs[0].artifacts_url?' runs_llvm.json`
 wget --output-document artifacts_flang-driver `jq -r '.workflow_runs[0].artifacts_url?' runs_flang-driver.json`
+
+# Retry with previous build in case no artifacts are available 
 echo "cat artifacts_llvm"
 cat artifacts_llvm
 i="0"
-set -x
 while [ `jq -r '.total_count?' artifacts_llvm` == "0" ] && [ $i -lt 3 ]
 do
-    echo "No artifacts in build $i, counting from latest" 
-    i=$[$i+1] 
-    wget --output-document artifacts_llvm `jq -r --argjson i "$i" '.workflow_runs[$i].artifacts_url?' runs_llvm.json` 
-    echo "cat artifacts_llvm" 
-    cat artifacts_llvm 
+echo "No artifacts in build $i, counting from latest"
+i=$[$i+1]
+wget --output-document artifacts_llvm `jq -r --argjson i "$i" '.workflow_runs[$i].artifacts_url?' runs_llvm.json`
+echo "cat artifacts_llvm"
+cat artifacts_llvm
 done
+
 echo "cat artifacts_flang-driver"
 cat artifacts_flang-driver
 i="0"
 while [ `jq -r '.total_count?' artifacts_flang-driver` == "0" ] && [ $i -lt 3 ]
 do
-    echo "No artifacts in build $i, counting from latest" 
-    i=$[$i+1] 
-    wget --output-document artifacts_flang-driver `jq -r --argjson i "$i" '.workflow_runs[$i].artifacts_url?' runs_flang-driver.json` 
-    echo "cat artifacts_flang-driver" 
-    cat artifacts_flang-driver 
+echo "No artifacts in build $i, counting from latest"
+i=$[$i+1]
+wget --output-document artifacts_flang-driver `jq -r --argjson i "$i" '.workflow_runs[$i].artifacts_url?' runs_flang-driver.json`
+echo "cat artifacts_flang-driver"
+cat artifacts_flang-driver
 done
-url=`jq -r '.artifacts[] | select(.name == "llvm_build_arm64") | .archive_download_url' artifacts_llvm`
-url=`jq -r '.artifacts[] | select(.name == "flang-driver_build_arm64") | .archive_download_url' artifacts_flang-driver`
-unzip llvm_build.zip
-tar xzf llvm_build.tar.gz
-cd llvm/build
-sudo make install/fast
-cd ../..
-unzip flang-driver_build.zip
-tar xzf flang-driver_build.tar.gz
-cd flang-driver/build
-sudo make install/fast
-flang --version
-CMAKE_OPTIONS="-DLLVM_TARGETS_TO_BUILD=OpenMP -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++"
-git clone --depth 1 --single-branch --branch release_90 https://github.com/llvm-mirror/openmp.git
-cd openmp
-mkdir -p build && cd build
-cmake $CMAKE_OPTIONS ..
-make -j$(nproc)
-make install
-mkdir -p build && cd build
-cmake -DLLVM_TARGETS_TO_BUILD=flang -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DCMAKE_Fortran_COMPILER=/usr/local/bin/flang -DCMAKE_Fortran_COMPILER_ID=Flang -DFLANG_INCLUDE_DOCS=ON -DFLANG_LLVM_EXTENSIONS=ON ke -j$(nproc)
-make install
+
+set -x
+
+url=`jq -r '.artifacts[] | select(.name == "llvm_build_ARM64") | .archive_download_url' artifacts_llvm`
+wget --output-document llvm_build.zip --header="Authorization: Bearer " $url
+
+url=`jq -r '.artifacts[] | select(.name == "flang-driver_build_ARM64") | .archive_download_url' artifacts_flang-driver`
+wget --output-document flang-driver_build.zip --header="Authorization: Bearer " $url
