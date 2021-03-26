f434474f1f071be10e82689c5249408f234517b5
Chirag Khandelwal <Chirag.Khandelwal@amd.com>
2021-03-26 13:24:59 +0530

    [Flang2] Support for vector always loop directive

--- test/directives/vector_directive3.f90
+++ test/directives/vector_directive3.f90
@@ -1,15 +1,22 @@
-! RUN: %flang -c %s 2>&1 | FileCheck %s -allow-empty --check-prefix=CHECK
+!! check for pragma support for !dir$ vector always
+!RUN: %flang -S -O2 -emit-llvm %s -o - | FileCheck %s
+!CHECK: define void @sumsimd_{{.*$}}
+!CHECK: {{.*}}!llvm.access.group ![[ACCGRP:[0-9]+]]
+!CHECK: vector.ph:{{.*}}
+!CHECK: vector.body:{{.*}}
+!CHECK: {{.*}}shufflevector{{.*}}
+!CHECK: {{.*}}add <2 x i64>{{.*}}
+!CHECK: {{.*}}"llvm.loop.parallel_accesses", ![[ACCGRP]]}
+!CHECK: {{.*}}"llvm.loop.isvectorized", i32 1{{.*}}
+!CHECK: {{.*}}"llvm.loop.unroll.runtime.disable"{{.*}}
 
-subroutine add(arr1,arr2,arr3,N)
-  integer :: i,N
-  integer :: arr1(N)
-  integer :: arr2(N)
-  integer :: arr3(N)
+SUBROUTINE sumsimd(myarr1,myarr2,ub)
+  INTEGER, POINTER :: myarr1(:)
+  INTEGER, POINTER :: myarr2(:)
+  INTEGER :: ub
 
-  !dir$ vector always
-  do i = 1, N
-    arr3(i) = arr1(i) - arr2(i)
-  end do
-end subroutine
-! CHECK-NOT: F90-S-0602
-! CHECK-NOT: F90-S-0603
+  !DIR$ VECTOR ALWAYS
+  DO i=1,ub
+      myarr1(i) = myarr1(i)+myarr2(i)
+  END DO
+END SUBROUTINE
