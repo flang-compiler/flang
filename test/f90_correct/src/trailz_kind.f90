38e7fd452aedf7aec2c51f280eb160e978915579
Kiran Chandramohan <kiran.chandramohan@arm.com>
2021-03-29 22:27:09 +0100

    [License] Switch to new LLVM License
    A few old PRs (listed below) which carried the old license
    were merged recently. This patch switches to the new license.
    
    https://github.com/flang-compiler/flang/pull/839
    https://github.com/flang-compiler/flang/pull/737
    https://github.com/flang-compiler/flang/pull/765
    https://github.com/flang-compiler/flang/pull/815

--- test/f90_correct/src/trailz_kind.f90
+++ test/f90_correct/src/trailz_kind.f90
@@ -1,4 +1,3 @@
-! Copyright (c) 2019, ARM Ltd.  All rights reserved.
 !
 ! Licensed under the Apache License, Version 2.0 (the "License");
 ! you may not use this file except in compliance with the License.
