flang-compiler/flang

commit 31d2ca5bc01f92d99299cd352af1eb9eb90eecae

[libpgmath] add several quad-precision FP intrisics.

runtime/libpgmath/lib/common/fltmanip.c
@@ -20,11 +20,24 @@
      - Brent
 */
 
-double __mth_i_dround(double x);
 float __mth_i_around(float x);
-double __mth_i_dremainder(double x, double y);
 float __mth_i_remainder(float x, float y);
+double __mth_i_dround(double x);
+double __mth_i_dremainder(double x, double y);
+#ifdef TARGET_SUPPORTS_QUADFP
+long double __mth_i_qround(long double x);
+long double __mth_i_qremainder(long double x, long double y);
+long double scalbnl(long double x, int i);
+#endif
 int __fenv_fegetzerodenorm(void);
+
+#ifdef TARGET_SUPPORTS_QUADFP
+long double
+__nearbyintl(long double x)
+{
+  return __mth_i_qround(x);
+}
+#endif
 
 double
 __nearbyint(double x)
@@ -38,6 +51,14 @@ __nearbyintf(float x)
   return __mth_i_around(x);
 }
 
+#ifdef TARGET_SUPPORTS_QUADFP
+long double
+__remainderl(long double x, long double y)
+{
+  return __mth_i_qremainder(x, y);
+}
+#endif
+
 double
 __remainder(double x, double y)
 {
@@ -49,6 +70,130 @@ __remainderf(float x, float y)
 {
   return __mth_i_remainder(x, y);
 }
+
+#ifdef TARGET_SUPPORTS_QUADFP
+static inline int
+isnan(unsigned int h, unsigned int j, unsigned int k, unsigned int l)
+{
+    return (((h & 0x7fff0000) == 0x7fff0000) &&
+           ((l | k | j | (h & 0xffff)) != 0));
+}
+#endif
+
+#define _MAXLONGDOUBLE (1.18973149535723176508575932662800702E+4932L)
+#define IX_SIZE 4 /* ix[] has 4 element and 128 bits */
+#define IZ_SIZE 2 /* iz[] has 4 element and 128 bits */
+#define GET_SIGN 31
+#define GET_EXP1 16
+#define GET_EXP2 23
+#define IS_ZERO(h, j, k, l) (((h) & 0x7fffffff) == 0) && (((l) | (k) | (j)) == 0)
+#define IS_MIN_NORM(h, j, k, l, s) (((h) & 0x7fffffff) == 0x00010000) && (((l) | (k) | (j)) == 0) && (s)
+#define IS_MAX_NORM(h, j, k, l ,s) (((h) & 0x7fffffff) == 0x7ffeffff) && (((~((l) & 0xffffffff)) | \
+                           (~((k) & 0xffffffff)) | (~((j) & 0xffffffff))) == 0) && (!(s))
+#ifdef TARGET_SUPPORTS_QUADFP
+long double
+__nextafterl(long double x, long double y)
+{
+  long double ex;
+  unsigned int ix[IX_SIZE], ixh, ixj, ixk, ixl, iyh, iyj, iyk, iyl;
+  unsigned long long iz[IZ_SIZE], izh, izl;
+  int idflag, subflag;
+
+  ex = y;
+  iyl = *((int*)(&(ex)));
+  iyk = *((int*)(&(ex)) + 1);
+  iyj = *((int*)(&(ex)) + 2);
+  iyh = *((int*)(&(ex)) + 3);
+
+  ex = x;
+  ixl = *((int*)(&(ex)));
+  ixk = *((int*)(&(ex)) + 1);
+  ixj = *((int*)(&(ex)) + 2);
+  ixh = *((int*)(&(ex)) + 3);
+
+  izl = *((long long *)(&(ex)));
+  izh = *((long long *)(&(ex)) + 1);
+
+  /* y is nan, return y */
+  if (isnan(iyh, iyj, iyk, iyl))
+    return y;
+
+  /* x is nan, return x */
+  if (isnan(ixh, ixj, ixk, ixl))
+    return x;
+
+  /* x infinity, return -_MAXLONGDOUBLE, +_MAXLONGDOUBLE, or x */
+  if ((ixh & 0x7fff0000) == 0x7fff0000) {
+    if ((iyh & 0x7fff0000) == 0x7fff0000) {
+      if ((ixh >> GET_SIGN) ^ (iyh >> GET_SIGN))
+        return (int)(ixh >> GET_SIGN) ? -_MAXLONGDOUBLE : _MAXLONGDOUBLE;
+      return x;
+    }
+    if (!(ixh & 0x80000000))
+      /* +inf, any number */
+      return _MAXLONGDOUBLE;
+    /* -inf, any number */
+    return -_MAXLONGDOUBLE;
+  }
+
+  if (x == y)
+    return x;
+  subflag = (x > y);
+
+  if (IS_ZERO(ixh, ixj, ixk ,ixl)) {
+    idflag = __fenv_fegetzerodenorm();
+    ix[3] = ((unsigned int) subflag << GET_SIGN) | ((unsigned int) idflag << GET_EXP1);
+    ix[2] = 0x00000000;
+    ix[1] = 0x00000000;
+    ix[0] = (unsigned int) !idflag;
+    ex = *((long double *)ix);
+    return x + ex; // underflow here
+  } else {
+    if (ixh & 0x80000000)
+      subflag = !subflag;
+    if (IS_MIN_NORM(ixh, ixj, ixk, ixl, subflag)) {
+      /* In this special case, setup a value to subtract
+         so we cause exceptions to occur properly
+      */
+      idflag = __fenv_fegetzerodenorm();
+      ix[3] = (ixh & 0x80000000);
+      ix[3] = ix[3] | ((unsigned int) idflag << GET_EXP2);
+      ix[2] = 0x00000000;
+      ix[1] = 0x00000000;
+      ix[0] = (unsigned int) !idflag;
+      ex = *((long double *)ix);
+      return x - ex; // possible underflow here
+    } else if (IS_MAX_NORM(ixh, ixj, ixk, ixl, subflag)) {
+      ix[3] = (ixh & 0xffca0000);
+      ix[2] = 0;
+      ix[1] = 0;
+      ix[0] = 0;
+      ex = *((long double *)ix);
+      return x + ex; // overflow to infinity
+    } else {
+      /* This is the normal case */
+        if (subflag) {
+          if (izl == 0) {
+            iz[0] = izl - 1;
+            iz[1] = izh - 1;
+          } else {
+            iz[0] = izl - 1;
+            iz[1] = izh;
+          }
+        } else {
+          iz[0] = izl + 1;
+          if (iz[0] == 0) {
+            iz[1] = izh + 1;
+          } else {
+            iz[1] = izh;
+        }
+      }
+    }
+  }
+  ex = *((long double *)iz);
+  return ex;
+}
+#endif
 
 #define _MAXDOUBLE (1.7976931348623157e+308)
 
@@ -255,6 +400,14 @@ __nextafterf(float x, float y)
   ex = *((float *)&iz);
   return ex;
 }
+
+#ifdef TARGET_SUPPORTS_QUADFP
+long double
+__scalbnl(long double x, int i)
+{
+  return scalbnl(x,i);
+}
+#endif
 
 double
 __scalbn(double x, int i)

runtime/libpgmath/lib/common/iqnint.c
@@ -1,0 +1,22 @@
+/*
+ * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+ * See https://llvm.org/LICENSE.txt for license information.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ */
+
+/* Intrinsic function which take quad precision arguments. */
+
+#include "mthdecls.h"
+/* INT */
+#define INT_MAX 2147483647
+int __mth_i_iqnint(long double d)
+{
+  if (d > 0 && (d > INT_MAX))
+    return INT_MAX;
+
+  if (d < 0 && (d < -INT_MAX - 1))
+    return -INT_MAX - 1;
+
+  return lroundl(d);
+}

runtime/libpgmath/lib/common/kqnint.c
@@ -1,0 +1,22 @@
+/*
+ * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+ * See https://llvm.org/LICENSE.txt for license information.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ */
+
+/* Intrinsic function which take quad precision arguments. */
+
+#include "mthdecls.h"
+/* INT */
+#define LONG_LONG_MAX 9223372036854775807
+long long __mth_i_kiqnint(long double d)
+{
+  if (d > 0 && (d > LONG_LONG_MAX))
+    return LONG_LONG_MAX;
+
+  if (d < 0 && (d < -LONG_LONG_MAX - 1))
+    return -LONG_LONG_MAX - 1;
+
+  return lroundl(d);
+}

CMakeLists.txt
@@ -43,6 +43,7 @@ elseif (${TARGET_ARCHITECTURE} MATCHES "^(aarch64|arm64)$")
   set(TARGET_ARCHITECTURE aarch64)
   set(ARCHNAME aarch64)
   set(ARCH ARM)
+  set(TARGET_SUPPORTS_QUADFP True)
 elseif( ${TARGET_ARCHITECTURE} STREQUAL "ppc64le" )
   set(ARCHNAME ppc64le)
   set(ARCH POWER)

runtime/libpgmath/CMakeLists.txt
@@ -61,6 +61,8 @@ if(${LIBPGMATH_SYSTEM_PROCESSOR} MATCHES "aarch64")
   else()
     message(STATUS "Libpgmath CPU target explicitly set to ${LLVM_FLANG_CPU_TARGET}")
   endif()
+  add_definitions(-DTARGET_SUPPORTS_QUADFP)
+  set(TARGET_SUPPORTS_QUADFP True)
 endif()
 
 if(CMAKE_C_COMPILER_ID STREQUAL "GNU" AND ${LIBPGMATH_SYSTEM_PROCESSOR} MATCHES "x86_64|aarch64")

runtime/libpgmath/lib/common/CMakeLists.txt
@@ -294,6 +294,15 @@ set(SRCS
   sign.c
   sind.c
   tand.c)
+
+set(SRCS_QUADFP
+  iqnint.c
+  kqnint.c)
+
+if(TARGET_SUPPORTS_QUADFP)
+  LIST(APPEND SRCS ${SRCS_QUADFP})
+endif()
+
 libmath_add_object_library("${SRCS}" "${FLAGS}" "${DEFINITIONS}" "")
 
 set(DEFINITIONS_CMPLX ${DEFINITIONS} MTH_CMPLX_C99_ABI)
@@ -449,7 +458,7 @@ endif()
 # Generate tmp-mth_statsdefs.h
 set(TARGET_NAME "tmp-mth_statsdefs")
 add_custom_command(OUTPUT ${TARGET_NAME}.h PRE_BUILD
-  COMMAND ${SH_PROGRAM} "${LIBPGMATH_TOOLS_DIR}/${TARGET_NAME}_${PROCESSOR}.sh" ${CMAKE_CURRENT_SOURCE_DIR} ${TARGET_NAME}.h)
+  COMMAND ${SH_PROGRAM} "${LIBPGMATH_TOOLS_DIR}/${TARGET_NAME}_${PROCESSOR}.sh" ${CMAKE_CURRENT_SOURCE_DIR} ${TARGET_NAME}.h ${TARGET_SUPPORTS_QUADFP})
 
 add_custom_target(${TARGET_NAME} ALL DEPENDS "${TARGET_NAME}.h")
 libmath_add_object_library("dispatch.c" "${FLAGS}" "${DEFINITIONS}" "dispatch")

runtime/libpgmath/lib/common/dispatch.c
@@ -179,6 +179,7 @@ static char *csv[] = {
         /* List needs to follow sv_e in mth_tbldefs.h */
         [sv_ss]         = "ss",
         [sv_ds]         = "ds",
+        [sv_qs]         = "qs",
         [sv_cs]         = "cs",
         [sv_zs]         = "zs",
         [sv_cv1]        = "cv1",
@@ -404,6 +405,8 @@ static  sv2attributes_t sv2attributes[] = {
 
         [sv_ds]     = { .elmtsz=elmtsz_64,     .nelmt=1},
         [sv_zs]     = { .elmtsz=elmtsz_64,     .nelmt=1},
+
+        [sv_qs]     = { .elmtsz=elmtsz_128,    .nelmt=1},
 
         [sv_sv4]    = { .elmtsz=elmtsz_128,    .nelmt=4*1},
         [sv_dv2]    = { .elmtsz=elmtsz_128,    .nelmt=2*1},

runtime/libpgmath/lib/common/mth_128defs.c
@@ -2084,6 +2084,38 @@ MTH_DISPATCH_FUNC(__pd_exp_2m)(vrd2_t x, vid2_t m)
   return (fptr(x, m));
 }
 
+#ifdef TARGET_SUPPORTS_QUADFP
+vrq1_t
+MTH_DISPATCH_FUNC(__fq_exp_1)(vrq1_t x)
+{
+  vrq1_t (*fptr)(vrq1_t);
+  _MTH_I_INIT();
+  _MTH_I_STATS_INC(func_exp, sv_qs, frp_f);
+  fptr = (vrq1_t(*)(vrq1_t))MTH_DISPATCH_TBL[func_exp][sv_qs][frp_f];
+  return (fptr(x));
+}
+
+vrq1_t
+MTH_DISPATCH_FUNC(__rq_exp_1)(vrq1_t x)
+{
+  vrq1_t (*fptr)(vrq1_t);
+  _MTH_I_INIT();
+  _MTH_I_STATS_INC(func_exp, sv_qs, frp_r);
+  fptr = (vrq1_t(*)(vrq1_t))MTH_DISPATCH_TBL[func_exp][sv_qs][frp_r];
+  return (fptr(x));
+}
+
+vrq1_t
+MTH_DISPATCH_FUNC(__pq_exp_1)(vrq1_t x)
+{
+  vrq1_t (*fptr)(vrq1_t);
+  _MTH_I_INIT();
+  _MTH_I_STATS_INC(func_exp, sv_qs, frp_p);
+  fptr = (vrq1_t(*)(vrq1_t))MTH_DISPATCH_TBL[func_exp][sv_qs][frp_p];
+  return (fptr(x));
+}
+#endif
+
 vrs1_t
 MTH_DISPATCH_FUNC(__fs_log_1)(vrs1_t x)
 {
@@ -3892,6 +3924,38 @@ MTH_DISPATCH_FUNC(__pd_aint_2m)(vrd2_t x, vid2_t m)
   fptr = (vrd2_t(*)())MTH_DISPATCH_TBL[func_aint][sv_dv2m][frp_p];
   return (fptr(x, m));
 }
+
+#ifdef TARGET_SUPPORTS_QUADFP
+vrq1_t
+MTH_DISPATCH_FUNC(__fq_aint_1)(vrq1_t x)
+{
+  vrq1_t (*fptr)(vrq1_t);
+  _MTH_I_INIT();
+  _MTH_I_STATS_INC(func_aint, sv_qs, frp_f);
+  fptr = (vrq1_t(*)(vrq1_t))MTH_DISPATCH_TBL[func_aint][sv_qs][frp_f];
+  return (fptr(x));
+}
+
+vrq1_t
+MTH_DISPATCH_FUNC(__rq_aint_1)(vrq1_t x)
+{
+  vrq1_t (*fptr)(vrq1_t);
+  _MTH_I_INIT();
+  _MTH_I_STATS_INC(func_aint, sv_qs, frp_r);
+  fptr = (vrq1_t(*)(vrq1_t))MTH_DISPATCH_TBL[func_aint][sv_qs][frp_r];
+  return (fptr(x));
+}
+
+vrq1_t
+MTH_DISPATCH_FUNC(__pq_aint_1)(vrq1_t x)
+{
+  vrq1_t (*fptr)(vrq1_t);
+  _MTH_I_INIT();
+  _MTH_I_STATS_INC(func_aint, sv_qs, frp_p);
+  fptr = (vrq1_t(*)(vrq1_t))MTH_DISPATCH_TBL[func_aint][sv_qs][frp_p];
+  return (fptr(x));
+}
+#endif
 
 vrs1_t
 MTH_DISPATCH_FUNC(__fs_ceil_1)(vrs1_t x)

commit d4a865809700f02f2c4312083c8f70358ed4d01a

[libpgmath] Fix several format issues
These format issues were introduced by commit `31d2ca5bc01`.

runtime/libpgmath/lib/common/fltmanip.c
@@ -100,16 +100,16 @@ __nextafterl(long double x, long double y)
   int idflag, subflag;
 
   ex = y;
-  iyl = *((int*)(&(ex)));
-  iyk = *((int*)(&(ex)) + 1);
-  iyj = *((int*)(&(ex)) + 2);
-  iyh = *((int*)(&(ex)) + 3);
+  iyl = *((int *)(&(ex)));
+  iyk = *((int *)(&(ex)) + 1);
+  iyj = *((int *)(&(ex)) + 2);
+  iyh = *((int *)(&(ex)) + 3);
 
   ex = x;
-  ixl = *((int*)(&(ex)));
-  ixk = *((int*)(&(ex)) + 1);
-  ixj = *((int*)(&(ex)) + 2);
-  ixh = *((int*)(&(ex)) + 3);
+  ixl = *((int *)(&(ex)));
+  ixk = *((int *)(&(ex)) + 1);
+  ixj = *((int *)(&(ex)) + 2);
+  ixh = *((int *)(&(ex)) + 3);
 
   izl = *((long long *)(&(ex)));
   izh = *((long long *)(&(ex)) + 1);

runtime/libpgmath/lib/common/iqnint.c
@@ -12,10 +12,10 @@
 #define INT_MAX 2147483647
 int __mth_i_iqnint(long double d)
 {
-  if (d > 0 && (d > INT_MAX))
+  if ((d > 0.0L) && (d > INT_MAX))
     return INT_MAX;
 
-  if (d < 0 && (d < -INT_MAX - 1))
+  if ((d < 0.0L) && (d < -INT_MAX - 1))
     return -INT_MAX - 1;
 
   return lroundl(d);

runtime/libpgmath/lib/common/kqnint.c
@@ -12,11 +12,11 @@
 #define LONG_LONG_MAX 9223372036854775807
 long long __mth_i_kiqnint(long double d)
 {
-  if (d > 0 && (d > LONG_LONG_MAX))
+  if ((d > 0.0L) && (d > LONG_LONG_MAX))
     return LONG_LONG_MAX;
 
-  if (d < 0 && (d < -LONG_LONG_MAX - 1))
-    return -LONG_LONG_MAX - 1;
+  if ((d < 0.0L) && (d < -LONG_LONG_MAX - 1LL))
+    return -LONG_LONG_MAX - 1LL;
 
   return lroundl(d);
 }