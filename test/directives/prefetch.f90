38dd597f19d5e2dd050b56535e527c6cdd005fbe
Abraham Tovar <abraham.tovar@mobica.com>
2021-09-27 11:58:00 +0200

    Adding changes to tests files to adapt to LLVM13 rules.

--- test/directives/prefetch.f90
+++ test/directives/prefetch.f90
@@ -1,4 +1,4 @@
-! RUN: %flang -O1 -S -emit-llvm %s -o - | FileCheck %s --check-prefix=CHECK-PREFETCH
+! RUN: %flang -O0 -S -emit-llvm %s -o - | FileCheck %s --check-prefix=CHECK-PREFETCH
 ! RUN: %flang -O1 -S -emit-llvm -Hy,59,0x4 %s -o - | FileCheck %s --check-prefix=CHECK-NOPREFETCH
 
 subroutine prefetch_dir(a1, a2)
@@ -15,9 +15,10 @@ end subroutine prefetch_dir
 ! CHECK-PREFETCH: [[a1:%[0-9]+]] = bitcast i64* %a1 to i8*
 ! CHECK-PREFETCH: [[a2:%[0-9]+]] = bitcast i64* %a2 to i8*
 ! CHECK-PREFETCH: [[a2base:%[0-9]+]] = getelementptr i8, i8* [[a2]], i64 1020
-! CHECK-PREFETCH: call void @llvm.prefetch{{.*}}(i8* [[a1]], i32 0, i32 3, i32 1)
-! CHECK-PREFETCH: [[i:%[0-9]+]] = shl nuw nsw i64 %indvars.iv, 2
-! CHECK-PREFETCH: [[a2elem:%[0-9]+]] = getelementptr i8, i8* [[a2base]], i64 [[i]]
+! CHECK-PREFETCH: [[i:%[0-9]+]] = load i32
+! CHECK-PREFETCH: [[TMP1:%[0-9]+]] = sext i32 [[i]] to i64
+! CHECK-PREFETCH: [[TMP2:%[0-9]+]] = mul nsw i64 [[TMP1]], 4
+! CHECK-PREFETCH: [[a2elem:%[0-9]+]] = getelementptr i8, i8* [[a2base]], i64 [[TMP2]]
 ! CHECK-PREFETCH: call void @llvm.prefetch{{.*}}(i8* [[a2elem]], i32 0, i32 3, i32 1)
 ! CHECK-PREFETCH: declare void @llvm.prefetch{{.*}}
 ! CHECK-NOPREFETCH-NOT: @llvm.prefetch
