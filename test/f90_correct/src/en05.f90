flang-compiler/flang

commit 1fbbb030e635a8325d65db99c3243c8d55266df2

Fix 2 errors of entry statement processing (#1036)
* Compiling error when return type of entry is complex/pointer.

  Main cause of this problem is that flang2 checks whether the return
  variable is used as a dummy argument only at the first entry point,
  so this patch fixes the problem by going through all entry points.
  In addition, because flang1 changes the storage class of a pointer
  dummy argument from `SC_DUMMY` to `SC_BASED`, this patch modifies the
  check for dummy argument in flang2 accordingly.

* Inconsistent with Fortran 2008 standard, ISO/IEC JTC 1/SC 22/WG 5/N1830
  section-12.6.2.6 (If the characteristics of the result of the function
  named in the ENTRY statement are the same as the characteristics of the
  result of the function named in the FUNCTION statement, their result
  variables identify the same variable, although their names need not be
  the same).

  Flang1 currently doesn’t handle functions that are converted to
  subroutines as the standard specifies, and all entry points simply use
  the same symbol for the return variable in the field `FVAL`. This patch
  takes into account all the symbols associated with the return variable,
  and performs symbol replacement in both the dummy argument list and the
  AST table.

tools/flang1/flang1exe/lowersym.c
@@ -1225,6 +1225,101 @@ lower_pointer_init(void)
   }
 } /* lower_pointer_init */
 
+/* When prepend_func_result_as_first_arg(semfin.c) has been called for an
+ * entry, the FVAL symbol and its descriptor symbol if exist are referred in
+ * the entry's dummy arguments.
+ * When we are going to identify all result variables of same dtype from
+ * different entry points with a single symbol, here we traverse all the dummy
+ * arguments, and replace the FVAL symbol and its descriptor symbol with this
+ * single symbol and corresponding descriptor symbol.
+ */
+static void
+replace_fval_in_params(SPTR entry, SPTR entrysame)
+{
+  SPTR fval, fvalsame, newdsc, newdscsame, newarg, newargsame;
+  int params, narg, i;
+
+  fval = FVALG(entry);
+  fvalsame = FVALG(entrysame);
+  newdsc = NEWDSCG(fval);
+  newarg = NEWARGG(fval);
+  newdscsame = NEWDSCG(fvalsame);
+  newargsame = NEWARGG(fvalsame);
+  params = DPDSCG(entry);
+  narg = PARAMCTG(entry);
+  for (i = 0; i < narg; i++) {
+    int arg = aux.dpdsc_base[params + i];
+    if (arg != 0 && arg == newarg) {
+      aux.dpdsc_base[params + i] = newargsame;
+      continue;
+    }
+    if (arg != 0 && arg == newdsc) {
+      aux.dpdsc_base[params + i] = newdscsame;
+      continue;
+    }
+  }
+}
+
+/* replace the symbol used in the ast of type A_ID taking advantage of the hash
+ * in the AST table
+ */
+static void
+replace_sptr_in_ast(SPTR sptr)
+{
+  SPTR newsptr;
+  int ast;
+
+  if (sptr <= NOSYM) {
+    return;
+  }
+  newsptr = LOWER_SYMBOL_REPLACE(sptr);
+  if (newsptr <= NOSYM) {
+    return;
+  }
+  ast = mk_id(sptr);
+  A_SPTRP(ast, newsptr);
+}
+
+static inline void
+add_replace_map(SPTR sptr, SPTR newsptr)
+{
+  if (sptr <= NOSYM || newsptr <= NOSYM) {
+    return;
+  }
+  LOWER_SYMBOL_REPLACE(sptr) = newsptr;
+}
+
+/* replace the fval symbol and associated symbols when the fval symbol is
+ * pointer or array
+ */
+static void
+replace_fval_in_ast(SPTR fval, SPTR fvalsame)
+{
+  SPTR var, var_same;
+
+  replace_sptr_in_ast(fval);
+
+  var = MIDNUMG(fval);
+  var_same = MIDNUMG(fvalsame);
+  add_replace_map(var, var_same);
+  replace_sptr_in_ast(var);
+
+  var = PTROFFG(fval);
+  var_same = PTROFFG(fvalsame);
+  add_replace_map(var, var_same);
+  replace_sptr_in_ast(var);
+
+  var = DESCRG(fval);
+  var_same = DESCRG(fvalsame);
+  add_replace_map(var, var_same);
+  replace_sptr_in_ast(var);
+
+  var = SDSCG(fval);
+  var_same = SDSCG(fvalsame);
+  add_replace_map(var, var_same);
+  replace_sptr_in_ast(var);
+}
+
 extern int pghpf_type_sptr;
 extern int pghpf_local_mode_sptr;
 
@@ -1232,6 +1327,8 @@ void
 lower_init_sym(void)
 {
   int sym, dtype;
+  LOGICAL from_func;
+
   lowersym.sc = SC_LOCAL;
   lowersym.parallel_depth = 0;
   lowersym.task_depth = 0;
@@ -1357,9 +1454,10 @@ lower_init_sym(void)
   stack_size = 100;
   NEW(stack, int, stack_size);
 
+  from_func = gbl.rutype == RU_SUBR && gbl.entries > NOSYM && FVALG(gbl.entries);
   /* look for ENTRY points; make all ENTRY points with the same
    * return type use the same FVAL symbol */
-  if (gbl.rutype == RU_FUNC) {
+  if (from_func || gbl.rutype == RU_FUNC) {
     int ent, esame;
     for (ent = gbl.entries; ent > NOSYM; ent = SYMLKG(ent)) {
       for (esame = gbl.entries; esame != ent; esame = SYMLKG(esame)) {
@@ -1367,11 +1465,13 @@ lower_init_sym(void)
         fval = FVALG(ent);
         fvalsame = FVALG(esame);
         if (fval && fvalsame && fval != fvalsame &&
-            DTYPEG(fval) == DTYPEG(fvalsame)) {
+            same_dtype(DTYPEG(fval), DTYPEG(fvalsame))) {
           /* esame is the earlier entry point, make ent use the
            * FVAL of esame */
           LOWER_SYMBOL_REPLACE(fval) = fvalsame;
+          replace_fval_in_params(ent, esame);
           FVALP(ent, fvalsame);
+          replace_fval_in_ast(fval, fvalsame);
           break; /* leave inner loop */
         }
       }

test/f90_correct/src/en04.f90
@@ -1,0 +1,91 @@
+!
+! Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+! See https://llvm.org/LICENSE.txt for license information.
+! SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+!
+! Test for all ENTRY points with the same return type identify the same variable
+
+function f1 ()
+  integer :: f1, e1
+entry e1 ()
+  e1 = 1
+end function
+
+function f2 ()
+  complex :: f2, e2
+entry e2 ()
+  e2 = (2, 2)
+end function
+
+function f3 ()
+  integer, pointer :: f3, e3
+entry e3 ()
+  allocate(e3)
+  e3 = 3
+end function
+
+function f4 ()
+  integer, dimension(4) :: f4, e4
+entry e4 ()
+  e4 = (/1,2,3,4/)
+end function
+
+function f5 ()
+  integer, dimension(:), pointer :: f5, e5
+entry e5 ()
+  allocate(e5(5))
+  e5 = (/1,2,3,4,5/)
+end function
+
+program test
+  interface
+    function f1 ()
+      integer :: f1
+    end function
+    function e1 ()
+      integer :: e1
+    end function
+    function f2 ()
+      complex :: f2
+    end function
+    function e2 ()
+      complex :: e2
+    end function
+    function f3 ()
+      integer, pointer :: f3
+    end function
+    function e3 ()
+      integer, pointer :: e3
+    end function
+    function f4 ()
+      integer, dimension(4) :: f4
+    end function
+    function e4 ()
+      integer, dimension(4) :: e4
+    end function
+    function f5 ()
+      integer, dimension(:), pointer :: f5
+    end function
+    function e5 ()
+      integer, dimension(:), pointer :: e5
+    end function
+  end interface
+
+  integer, parameter :: n = 10
+  integer :: rslts(n), expect(n)
+
+  rslts = 0
+  expect = 1
+  if (f1() == 1) rslts(1) = 1
+  if (e1() == 1) rslts(2) = 1
+  if (f2() == (2, 2)) rslts(3) = 1
+  if (e2() == (2, 2)) rslts(4) = 1
+  if (f3() == 3) rslts(5) = 1
+  if (e3() == 3) rslts(6) = 1
+  if (all(f4() == (/1,2,3,4/))) rslts(7) = 1
+  if (all(e4() == (/1,2,3,4/))) rslts(8) = 1
+  if (all(f5() == (/1,2,3,4,5/))) rslts(9) = 1
+  if (all(e5() == (/1,2,3,4,5/))) rslts(10) = 1
+
+  call check(rslts, expect, n)
+end program

test/f90_correct/inc/en03.mk
@@ -1,0 +1,28 @@
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+
+########## Make rule for test en03  ########
+
+
+en03: run
+
+
+build:  $(SRC)/en03.f90
+	-$(RM) en03.$(EXESUFFIX) core *.d *.mod FOR*.DAT FTN* ftn* fort.*
+	@echo ------------------------------------ building test $@
+	-$(CC) -c $(CFLAGS) $(SRC)/check.c -o check.$(OBJX)
+	-$(FC) -c $(FFLAGS) $(LDFLAGS) $(SRC)/en03.f90 -o en03.$(OBJX)
+	-$(FC) $(FFLAGS) $(LDFLAGS) en03.$(OBJX) check.$(OBJX) $(LIBS) -o en03.$(EXESUFFIX)
+
+
+run:
+	@echo ------------------------------------ executing test en03
+	en03.$(EXESUFFIX)
+
+verify: ;
+
+en03.run: run
+

test/f90_correct/inc/en04.mk
@@ -1,0 +1,23 @@
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+
+$(TEST): run
+
+
+build:  $(SRC)/$(TEST).f90
+	-$(RM) $(TEST).$(EXESUFFIX) core *.d *.mod FOR*.DAT FTN* ftn* fort.*
+	@echo ------------------------------------ building test $@
+	-$(CC) -c $(CFLAGS) $(SRC)/check.c -o check.$(OBJX)
+	-$(FC) -c $(FFLAGS) $(LDFLAGS) $(SRC)/$(TEST).f90 -o $(TEST).$(OBJX)
+	-$(FC) $(FFLAGS) $(LDFLAGS) $(TEST).$(OBJX) check.$(OBJX) $(LIBS) -o $(TEST).$(EXESUFFIX)
+
+
+run:
+	@echo ------------------------------------ executing test $(TEST)
+	$(TEST).$(EXESUFFIX)
+
+verify: ;
+

test/f90_correct/lit/en03.sh
@@ -1,0 +1,9 @@
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Shared lit script for each tests. Run bash commands that run tests with make.
+
+# RUN: KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %S/runmake | tee %t
+# RUN: cat %t | FileCheck %S/runmake

test/f90_correct/lit/en04.sh
@@ -1,0 +1,9 @@
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Shared lit script for each tests. Run bash commands that run tests with make.
+
+# RUN: KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %S/runmake | tee %t
+# RUN: cat %t | FileCheck %S/runmake

test/f90_correct/src/en03.f90
@@ -1,0 +1,59 @@
+!
+! Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+! See https://llvm.org/LICENSE.txt for license information.
+! SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+!
+! Test entry for cases:
+! 1) Function return complex, entry return integer
+! 2) All entries return pointer
+
+function f1()
+  complex :: f1
+  integer :: e1
+  f1 = (1, 1)
+  return
+entry e1()
+  e1 = 2
+  return
+end function
+
+function f2()
+  integer, pointer :: f2, e2
+  allocate(f2)
+  f2 = 3
+  return
+entry e2()
+  allocate(e2)
+  e2 = 4
+  return
+end function
+
+program test
+  interface
+    function f1()
+      complex :: f1
+    end function
+    function e1()
+      integer :: e1
+    end function
+    function f2()
+      integer, pointer :: f2
+    end function
+    function e2()
+      integer, pointer :: e2
+    end function
+  end interface
+
+  integer, parameter :: n = 4
+  integer :: rslts(n), expect(n)
+
+  rslts = 0
+  expect = 1
+
+  if(f1() .eq. (1, 1)) rslts(1) = 1
+  if(e1() .eq. 2) rslts(2) = 1
+  if(f2() .eq. 3) rslts(3) = 1
+  if(e2() .eq. 4) rslts(4) = 1
+
+  call check(rslts, expect, n)
+end program

tools/flang1/flang1exe/semfin.c
@@ -467,10 +467,13 @@ semfin(void)
             error(45, 3, gbl.lineno, SYMNAME(sptr), SYMNAME(gbl.entries));
         pointer_check:
           STYPEP(FVALG(sptr), ST_VAR);
-          if (POINTERG(sptr) || ALLOCATTRG(FVALG(sptr))) {
+          if (POINTERG(FVALG(sptr)) || ALLOCATTRG(FVALG(sptr))) {
             /* We convert a pointer-valued function into a subroutine whose
              * first dummy argument is the result now, really late in
              * semantic analysis.
+             * Check the attributes of fval instead of the attributes of entry,
+             * because only the first entry can get all attributes defined by
+             * fval through copy_type_to_entry(semant.c).
              */
             prepend_func_result_as_first_arg(sptr);
             gbl.rutype = RU_SUBR;

commit 4fcb29bd54c6b3e0ff3070d666585ef075480b53

Fix for regression introduced by PR #1036 (issue #1137).
   When the entry returns adjustable length character without POINTER attribute,
   the field NEWARG of the return variable is not set which causes that
   the return variable in dummy arguments is not replaced,
   and the field CVLEN of the return variable also needs to be replaced.

tools/flang1/flang1exe/lowersym.c
@@ -1239,21 +1239,32 @@ replace_fval_in_params(SPTR entry, SPTR entrysame)
   SPTR fval, fvalsame, newdsc, newdscsame, newarg, newargsame;
   int params, narg, i;
 
+  narg = PARAMCTG(entry);
+  if (narg == 0)
+    return;
+  params = DPDSCG(entry);
   fval = FVALG(entry);
   fvalsame = FVALG(entrysame);
   newdsc = NEWDSCG(fval);
   newarg = NEWARGG(fval);
+  /* If the return variable is an adjustable length character without POINTER
+   * attribute, its NEWARG keeps SPTR_NULL(see init_newargs in bblock.c), and
+   * itself will be put into dummy parameters(see newargs_for_entry in
+   * dpm_out.c), so we match and replace the return variable directly.
+   */
+  if (newarg == SPTR_NULL)
+    newarg = fval;
   newdscsame = NEWDSCG(fvalsame);
   newargsame = NEWARGG(fvalsame);
-  params = DPDSCG(entry);
-  narg = PARAMCTG(entry);
+  if (newargsame == SPTR_NULL)
+    newargsame = fvalsame;
   for (i = 0; i < narg; i++) {
     int arg = aux.dpdsc_base[params + i];
-    if (arg != 0 && arg == newarg) {
+    if (arg != SPTR_NULL && arg == newarg) {
       aux.dpdsc_base[params + i] = newargsame;
       continue;
     }
-    if (arg != 0 && arg == newdsc) {
+    if (arg != SPTR_NULL && arg == newdsc) {
       aux.dpdsc_base[params + i] = newdscsame;
       continue;
     }
@@ -1316,6 +1327,11 @@ replace_fval_in_ast(SPTR fval, SPTR fvalsame)
 
   var = SDSCG(fval);
   var_same = SDSCG(fvalsame);
+  add_replace_map(var, var_same);
+  replace_sptr_in_ast(var);
+
+  var = CVLENG(fval);
+  var_same = CVLENG(fvalsame);
   add_replace_map(var, var_same);
   replace_sptr_in_ast(var);
 }

test/f90_correct/inc/en05.mk
@@ -1,0 +1,22 @@
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+
+$(TEST): run
+
+
+build:  $(SRC)/$(TEST).f90
+	-$(RM) $(TEST).$(EXESUFFIX) core *.d *.mod FOR*.DAT FTN* ftn* fort.*
+	@echo ------------------------------------ building test $@
+	-$(FC) -c $(FFLAGS) $(LDFLAGS) $(SRC)/$(TEST).f90 -o $(TEST).$(OBJX)
+	-$(FC) $(FFLAGS) $(LDFLAGS) $(TEST).$(OBJX) $(LIBS) -o $(TEST).$(EXESUFFIX)
+
+
+run:
+	@echo ------------------------------------ executing test $(TEST)
+	$(TEST).$(EXESUFFIX)
+
+verify: ;
+

test/f90_correct/lit/en05.sh
@@ -1,0 +1,9 @@
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Shared lit script for each tests. Run bash commands that run tests with make.
+
+# RUN: KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %S/runmake | tee %t
+# RUN: cat %t | FileCheck %S/runmake

test/f90_correct/src/en04.f90
@@ -37,6 +37,21 @@ entry e5 ()
   e5 = (/1,2,3,4,5/)
 end function
 
+function f6 (n)
+  integer :: n
+  character(n) :: f6, e6
+entry e6 (n)
+  e6 = "hello1"
+end function
+
+function f7 (n)
+  integer :: n
+  character(n), pointer :: f7, e7
+entry e7 (n)
+  allocate(e7)
+  e7 = "hello2"
+end function
+
 program test
   interface
     function f1 ()
@@ -69,10 +84,27 @@ program test
     function e5 ()
       integer, dimension(:), pointer :: e5
     end function
+    function f6 (n)
+      integer :: n
+      character(n) :: f6
+    end function
+    function e6 (n)
+      integer :: n
+      character(n) :: e6
+    end function
+    function f7 (n)
+      integer :: n
+      character(n), pointer :: f7
+    end function
+    function e7 (n)
+      integer :: n
+      character(n), pointer :: e7
+    end function
   end interface
 
-  integer, parameter :: n = 10
+  integer, parameter :: n = 14
   integer :: rslts(n), expect(n)
+  character(6), pointer :: p
 
   rslts = 0
   expect = 1
@@ -86,6 +118,18 @@ program test
   if (all(e4() == (/1,2,3,4/))) rslts(8) = 1
   if (all(f5() == (/1,2,3,4,5/))) rslts(9) = 1
   if (all(e5() == (/1,2,3,4,5/))) rslts(10) = 1
+  if (len(f6(6)) == 6 .and. f6(6) == "hello1") rslts(11) = 1
+  if (len(e6(6)) == 6 .and. e6(6) == "hello1") rslts(12) = 1
+  p => f7(6)
+  if (associated(p)) then
+    if (len(p) == 6 .and. p == "hello2") rslts(13) = 1
+    deallocate(p)
+  endif
+  p => e7(6)
+  if (associated(p)) then
+    if (len(p) == 6 .and. p == "hello2") rslts(14) = 1
+    deallocate(p)
+  endif
 
   call check(rslts, expect, n)
 end program

test/f90_correct/src/en05.f90
@@ -1,0 +1,84 @@
+!
+! Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+! See https://llvm.org/LICENSE.txt for license information.
+! SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+!
+! Test the fix for introduced error, in which the entry returns adjustable
+! length character.
+
+function f1(n)
+  integer :: n
+  character(n) :: f1, e1
+  f1 = "AB"
+  return
+entry e1(n)
+  e1 = "XYZ"
+end function
+
+function f2(n)
+  integer :: n
+  character(n), pointer :: f2, e2
+  allocate(f2)
+  f2 = "AB"
+  return
+entry e2(n)
+  allocate(e2)
+  e2 = "XYZ"
+end function
+
+function f3(n)
+  integer :: n
+  character(n), dimension(2, 2) :: f3, e3
+  f3 = "AB"
+  return
+entry e3(n)
+  e3 = "XYZ"
+end function
+
+program p
+  interface
+    function f1(n)
+      integer :: n
+      character(n) :: f1
+    end function
+    function e1(n)
+      integer :: n
+      character(n) :: e1
+    end function
+    function f2(n)
+      integer :: n
+      character(n), pointer :: f2
+    end function
+    function e2(n)
+      integer :: n
+      character(n), pointer :: e2
+    end function
+    function f3(n)
+      integer :: n
+      character(n), dimension(2, 2) :: f3
+    end function
+    function e3(n)
+      integer :: n
+      character(n), dimension(2, 2) :: e3
+    end function
+  end interface
+  character(:), pointer :: cp
+
+  if (len(f1(2)) /= 2 .or. f1(2) /= "AB") STOP 1
+  if (len(e1(3)) /= 3 .or. e1(6) /= "XYZ") STOP 2
+
+  cp => f2(2)
+  if (.not. associated(cp)) STOP 3
+  if (len(cp) /= 2 .or. cp /= "AB") STOP 4
+  deallocate(cp)
+
+  cp => e2(3)
+  if (.not. associated(cp)) STOP 5
+  if (len(cp) /= 3 .or. cp /= "XYZ") STOP 6
+  deallocate(cp)
+
+  if (len(f3(2)) /= 2 .or. any(f3(2) /= "AB")) STOP 7
+  if (len(e3(3)) /= 3 .or. any(e3(3) /= "XYZ")) STOP 8
+
+  print *, "PASS"
+end program