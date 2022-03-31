26594e9aec75dac748f6c0c31f771da60999498d
Maciej Gabka <maciej.gabka@arm.com>
2022-03-31 11:18:18 +0100

    Set dwarf version to 4 in test/debug_info/redundant_inst.f90
    The LLVM14 uses dwarf version 5 by default, but this test
    was only compatible with older dwarf version.

--- test/debug_info/redundant_inst.f90
+++ test/debug_info/redundant_inst.f90
@@ -1,5 +1,5 @@
 !RUN: %flang %s -g -S -emit-llvm -o - | FileCheck %s --check-prefix=STORE
-!RUN: %flang %s -g -S -emit-llvm -o - | llc -O0 -fast-isel=true -filetype=obj -o %t
+!RUN: %flang %s -g -gdwarf-4 -S -emit-llvm -o - | llc -O0 -fast-isel=true -filetype=obj -o %t
 !RUN: llvm-dwarfdump --debug-line %t | FileCheck %s --check-prefix=LINETABLE
 
 !Check that `store` instruction is getting emitted for the second assignment.
