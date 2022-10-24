675ea9507fb6c9fb8ac01557dcbdf61bccddc471
Gabriel Baraldi <baraldigabriel@gmail.com>
2022-10-24 15:21:28 -0300

    Add test for pgmath veclib assertion failure with sincos.

---
+++ test/f90_correct/src/sincos.f90
@@ -0,0 +1,12 @@
+! Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+! See https://llvm.org/LICENSE.txt for license information.
+! SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+!
+
+program foo
+      real(kind=8) :: val, res1, res2
+      read *, val
+      res1 = sin(val)
+      res2 = cos(val)
+      print *, res1, res2
+end program foo
