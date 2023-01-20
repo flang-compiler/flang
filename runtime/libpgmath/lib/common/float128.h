3a480e9517914a545ff533bab0fa60d2ab24c7ae
Adam Kallai <kadam@inf.u-szeged.hu>
2023-01-20 17:02:16 +0100

    win: Remove '-mlong-double-128' compiler option
    On Windows 'long double' type size is 64 bits, same as double.
    As a workaround we started to use clang's '-mlong-double-128' option
    to force 'long double' to be 128 bits.
    With newer version of Windows SDK it causes an assertion in one of
    system header.
    Currently it breaks the Windows build, so it needs to remove.
    
    Issue: #1334

--- runtime/libpgmath/lib/common/float128.h
+++ runtime/libpgmath/lib/common/float128.h
@@ -24,7 +24,16 @@ typedef __float128 float128_t;
 typedef _Complex float __attribute__((mode(KC))) quad_complex_t;
 #else
 /* __float128 is not available on AArch64 or other generic targets;
-   on AArch64 at least, long double is 128 bits in size. */
+ * on AArch64 at least, long double is 128 bits in size.
+ *
+ * FIXME: On Windows long double size is 64 bits, same as double is.
+ * As workaround we used clang's '-mlong-double-128' compiler option
+ * to force long double to be 128 bits.
+ * With newer Windows SDK it causes an assertion in one of system header.
+ * Until the final solution we should use float128_t type with this limitation
+ * on Windows.
+ * Please see the issue: https://github.com/flang-compiler/flang/issues/1334
+ */
 typedef long double float128_t;
 typedef long double _Complex quad_complex_t;
 #endif
