88c8ea4119a52b4a8cc72de2b4316af80e5872c7
Adam Kallai <kadam@inf.u-szeged.hu>
2023-07-06 12:48:15 +0200

    Add Windows support to f90_correct tests
    The tests direct their outputs to a file and stdout by using tee,
    on Windows seems we have a buffering issue since the results of the test
    doesn't appeare on stdout. As workaround we used fflush in check.c.

--- test/f90_correct/lit/move_alloc.sh
+++ test/f90_correct/lit/move_alloc.sh
@@ -6,5 +6,5 @@
 
 # Shared lit script for each tests. Run bash commands that run tests with make.
 
-# RUN: KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %S/runmake | tee %t
+# RUN: env KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%/s MAKE_FILE_DIR=%/S/.. bash %/S/runmake | tee %/t
 # RUN: cat %t | FileCheck %S/runmake
--- test/f90_correct/lit/qsign.sh
+++ test/f90_correct/lit/qsign.sh
@@ -6,6 +6,6 @@
 # Shared lit script for each tests. Run bash commands that run tests with make.
 
 # REQUIRES: quadfp
-# RUN: KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %S/runmake | tee %t
+# RUN: env KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%/s MAKE_FILE_DIR=%/S/.. bash %/S/runmake | tee %/t
 # RUN: cat %t | FileCheck %S/runmake
 
