762e596b31bd79d833d3cc3edc9c8454d348c3bc
Bryan Chan <bryan.chan@huawei.com>
2023-10-04 08:34:38 -0400

    [DebugInfo][test] Fix test for LLVM 17 compatibility
    One of the test/debug_info tests was not updated when commit 08b46d71fc63 moved
    the DIImportedEntity list from the DICompileUnit node to the DISubprogram
    nodes.

--- test/debug_info/debug_module_import.f90
+++ test/debug_info/debug_module_import.f90
@@ -1,9 +1,8 @@
-!
 ! Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 ! See https://llvm.org/LICENSE.txt for license information.
 ! SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-!
 
+! REQUIRES: llvm-17
 ! RUN: %flang -g -S -emit-llvm %s -o - | FileCheck %s
 module first
 integer :: var1 = 37
@@ -27,9 +26,11 @@ end program hello
 ! CHECK-DAG: ![[DBG_MOD1:[0-9]+]] = !DIModule({{.*}}, name: "first"
 ! CHECK-DAG: ![[DBG_DIC:[0-9]+]] = distinct !DICompileUnit({{.*}}, imports: ![[DBG_IMPORTS:[0-9]+]], nameTableKind: None
 ! CHECK-DAG: ![[DBG_MOD2:[0-9]+]] = !DIModule({{.*}}, name: "second"
-! CHECK-DAG: ![[DBG_IMPORTS]] = !{![[DBG_IE1:[0-9]+]], ![[DBG_IE2:[0-9]+]], ![[DBG_IE3:[0-9]+]]}
-! CHECK-DAG: ![[DBG_IE1]] = !DIImportedEntity(tag: DW_TAG_imported_module, scope: ![[DBG_SP1:[0-9]+]], entity: ![[DBG_MOD1]],
-! CHECK-DAG: ![[DBG_SP1]] = distinct !DISubprogram(name: "init", scope: ![[DBG_MOD2]]
-! CHECK-DAG: ![[DBG_IE2]] = !DIImportedEntity(tag: DW_TAG_imported_module, scope: ![[DBG_SP2:[0-9]+]], entity: ![[DBG_MOD1]],
-! CHECK-DAG: ![[DBG_SP2]] = distinct !DISubprogram(name: "hello", scope: ![[DBG_DIC]]
-! CHECK-DAG: ![[DBG_IE3]] = !DIImportedEntity(tag: DW_TAG_imported_module, scope: ![[DBG_SP2]], entity: ![[DBG_MOD2]],
+! CHECK-DAG: ![[DBG_IMPORTS]] = !{}
+! CHECK-DAG: ![[DBG_IE1:[0-9]+]] = !DIImportedEntity(tag: DW_TAG_imported_module, scope: ![[DBG_SP1:[0-9]+]], entity: ![[DBG_MOD1]],
+! CHECK-DAG: ![[DBG_SP1]] = distinct !DISubprogram(name: "init", scope: ![[DBG_MOD2]], {{.*}}, retainedNodes: ![[RETAIN1:[0-9]+]]
+! CHECK-DAG: ![[RETAIN1]] = !{![[DBG_IE1]]}
+! CHECK-DAG: ![[DBG_IE2:[0-9]+]] = !DIImportedEntity(tag: DW_TAG_imported_module, scope: ![[DBG_SP2:[0-9]+]], entity: ![[DBG_MOD1]],
+! CHECK-DAG: ![[DBG_IE3:[0-9]+]] = !DIImportedEntity(tag: DW_TAG_imported_module, scope: ![[DBG_SP2]], entity: ![[DBG_MOD2]],
+! CHECK-DAG: ![[DBG_SP2]] = distinct !DISubprogram(name: "hello", scope: ![[DBG_DIC]], {{.*}}, retainedNodes: ![[RETAIN2:[0-9]+]]
+! CHECK-DAG: ![[RETAIN2]] = !{![[DBG_IE2]], ![[DBG_IE3]]}
